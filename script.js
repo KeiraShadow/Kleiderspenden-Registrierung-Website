d87ac3bc6a2340f926a7a536d2a2d19c507f9909
KeiraShadow <100292970+KeiraShadow@users.noreply.github.com>
2023-04-07 00:45:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 // Start at the top of the page
 window.scrollTo( (document.top))
 
@@ -11,7 +12,37 @@ $(document).ready(function() {
         // For example, show an alert message
         alert('Danke!');
     });
-   
+    /*$('#submit2').click(function() {
+        // Code to be executed when the button is clicked
+        // For example, show an alert message
+        alert('Danke!');
+    });*/
+    
+    /*
+    $('#submit2').click(function() {
+        // Holen Sie die Werte aus dem Formular
+        var artDerKleidung = $('#type2').val();
+        var kriesengebiet = $('#area2').val();
+        var StraseHausnummer = $('#straseHausnummer2').val();
+        var Stadt = $('#stadt2').val();
+        var postleitzahl = $('#postleitzahl2').val();
+      
+        // Holen Sie die aktuelle Zeit und das Datum
+        var currentTime = new Date();
+        var currentDateTime = currentTime.toLocaleString();
+      
+        // Erstellen Sie eine Ausgabezeichenfolge mit den Werten und der aktuellen Zeit und Datum
+        var outputString = "Art der Kleidung: " + artDerKleidung + "<br>" +
+                           "Kriesengebiet: " + kriesengebiet + "<br>" +
+                           "Straße & Hausnummer: " + StraseHausnummer + "<br>" +
+                           "Stadt: " + Stadt + "<br>" +
+                           "Postleitzahl: " + postleitzahl + "<br>" +
+                           "Aktuelle Zeit und Datum: " + currentDateTime;
+      
+        // Fügen Sie die Ausgabezeichenfolge zum div-Element "output" hinzu
+        $('#Ausgabe2').html(outputString);
+      }); */
+      
       $('#submit2').click(function() {
         // Holen Sie die Werte aus dem Formular
         var artDerKleidung = $('#type2').val();
@@ -35,5 +66,8 @@ $(document).ready(function() {
         // Zeigen Sie die Ausgabezeichenfolge als Alert-Nachricht an
         alert(outputString);
     });
+    
+
+
   });
 ;
